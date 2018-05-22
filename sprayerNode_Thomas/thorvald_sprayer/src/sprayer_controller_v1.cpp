1fd08aca78026a431d4b742f4bd7bb469a78fcc7
Thomas <thomas.degallaix@isen.yncrea.fr>
2018-05-22 17:34:49 +0200

    Working version of sprayerV3 with a service

--- sprayerNode_Thomas/thorvald_sprayer/src/sprayer_controller_v1.cpp
+++ sprayerNode_Thomas/thorvald_sprayer/src/sprayer_controller_v1.cpp
@@ -1,1 +1,33 @@
 #include "ros/ros.h"
+#include <thorvald_sprayer/sprayer_controller.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+  ros::init(argc,argv,"sprayer_controller_v1");
+
+  if(argc != 2 ) {
+    ROS_WARN("Usage: %s [Action to perform]", argv[0]);
+    ROS_WARN("Action to perform: \n    - [init]\n    - [run]\n    - [break]\r");
+    return 1;
+  }
+
+  ros::NodeHandle nh;
+  ros::ServiceClient client = nh.serviceClient<thorvald_sprayer::sprayer_controller>("sprayer_controller");
+
+  thorvald_sprayer::sprayer_controller srv;
+
+  srv.request.order = argv[1];
+  ROS_INFO("Sending %s order...", argv[1]);
+
+  if (client.call(srv)) {
+    ROS_INFO("ACK: %s", srv.response.message.c_str());
+  }
+  else {
+    ROS_ERROR("Failed to call service");
+    return 1;
+  }
+
+  return 0;
+}
--- sprayerNode_Thomas/thorvald_sprayer/src/sprayer_node_v3.cpp
+++ sprayerNode_Thomas/thorvald_sprayer/src/sprayer_node_v3.cpp
@@ -1,1 +1,133 @@
 #include "ros/ros.h"
+#include <thorvald_sprayer/CANFrame.h>
+#include <thorvald_sprayer/sprayer_controller.h>
+#include "std_msgs/String.h"
+#include <cstring>
+
+//https://github.com/g/roboteq
+
+using namespace std;
+
+/*Global variable used to store the service request*/
+string request;
+
+/*List of used TPDO, for more informations look at the roboteq datasheet*/
+enum RPDO {rpdo1=0x200,rpdo2=0x300};
+
+/*Procedure used to have access to the content of the service request*/
+void getRequest(string *request, thorvald_sprayer::sprayer_controller::Request req) {
+  *request = req.order.c_str();
+}
+
+/*callback for the service client*/
+bool serviceCallback(thorvald_sprayer::sprayer_controller::Request &req, thorvald_sprayer::sprayer_controller::Response &res) {
+  getRequest(&request,req);
+  if(strcmp(req.order.c_str(),"init") == 0) {
+    res.message = "Init order was given";
+  }
+  else if(strcmp(req.order.c_str(),"run") == 0) {
+    res.message = "Run order was given";
+  }
+  else if(strcmp(req.order.c_str(),"break") == 0) {
+    res.message = "Break order was given";
+  }
+  else {
+    res.message = "ERR: Something went wrong with the order !";
+  }
+  return true;
+}
+
+
+/*Function used to fulfill the message*/
+thorvald_sprayer::CANFrame fulfill_message(thorvald_sprayer::CANFrame *msg, int rpdo, int node_id, int var_1, int var_2) {
+
+  msg->can_id = rpdo + node_id;
+
+  /*Initialing the data array*/
+  for(int i=0;i<8;i++) {
+    msg->data[i] = 0;
+  }
+  msg->data[0] = var_1;
+  msg->data[3] = var_2;
+  msg->length = 8;
+
+  return *msg;
+}
+
+/*Function used to create the correct message according to the action asked by the user*/
+thorvald_sprayer::CANFrame process_data(thorvald_sprayer::CANFrame *msg, RPDO rpdo, string request, char** argv) {
+
+  if(strcmp(request.c_str(),"init") == 0) {
+    fulfill_message(msg,rpdo,atoi(argv[1]),1,0);
+  }
+  else if(strcmp(request.c_str(),"run") == 0) {
+    fulfill_message(msg,rpdo,atoi(argv[1]),2,0);
+  }
+  else if(strcmp(request.c_str(),"break") == 0) {
+    fulfill_message(msg,rpdo,atoi(argv[1]),0,1);
+  }
+  else{
+    ROS_WARN("/!\\ Doesn't know which action has to be performed /!\\");
+  }
+
+  return *msg;
+}
+
+/*Simple procedure which displays some informations*/
+void display_infos(thorvald_sprayer::CANFrame *msg, int count, string request) {
+  /*Display infos*/
+  if(request.c_str() == "init" || "run" || "break") {
+    ROS_INFO("#%d Sending data to RPDO1", count);
+  }
+  else{
+    ROS_INFO("#%d Sending data to RPDO2", count);
+  }
+  ROS_INFO("ID: %d", msg->can_id);
+  ROS_INFO("Length: %d", msg->length);
+  ROS_INFO("Data: [%d,%d]", msg->data[0], msg->data[3]);
+}
+
+
+int main(int argc, char **argv) {
+
+  /*Initializing the node*/http://wiki.ros.org/Services
+  ros::init(argc,argv,"sprayer_node_v3");
+
+  if(argc != 2 ) {
+    ROS_WARN("Usage: %s [NODE_id]", argv[0]);
+    return 1;
+  }
+
+  /*Start the node*/
+  ros::NodeHandle nh;
+
+  ros::ServiceServer serviceServer = nh.advertiseService("sprayer_controller",serviceCallback);
+  ros::Publisher sprayer_pub = nh.advertise<thorvald_sprayer::CANFrame>("/can_frames_device_t",1000);
+
+  /*Sleep at a rate of 1Hz*/
+  ros::Rate loop_rate(1);
+
+  int count = 0;
+  while(ros::ok()) {
+
+    /*Instantiate a new message*/
+    thorvald_sprayer::CANFrame msg;
+
+    RPDO rpdo;
+    /*What RPDO are we using ?*/
+    request.c_str() == "init" || "run" || "break" ? rpdo=rpdo1 : rpdo=rpdo2;
+
+    process_data(&msg, rpdo, request, argv);
+    display_infos(&msg, count, request);
+
+    sprayer_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    count++;
+
+  }
+
+  return 0;
+}
